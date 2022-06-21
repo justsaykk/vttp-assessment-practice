f6ce6c260f1f5f5c2fb6c6e81b8f456b56659a86
justsaykk <kaikein.woo@gmail.com>
2022-06-21 16:54:32 +0800

    initial commit - task 1 completed

---
+++ practice/src/main/java/pastyearpaper/App.java
@@ -0,0 +1,13 @@
+package pastyearpaper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
