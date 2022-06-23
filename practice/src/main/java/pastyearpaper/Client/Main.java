6a892b60613c7de614992400e7133af9ae7b7bc1
justsaykk <kaikein.woo@gmail.com>
2022-06-23 14:36:29 +0800

    moved httpwriter

--- practice/src/main/java/pastyearpaper/Client/Main.java
+++ practice/src/main/java/pastyearpaper/Client/Main.java
@@ -5,7 +5,6 @@ import java.net.UnknownHostException;
 import java.util.*;
 
 import pastyearpaper.Server.HttpClientConnection;
-import pastyearpaper.Server.HttpServer;
 
 public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException {
